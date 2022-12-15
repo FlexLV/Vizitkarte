584e82236ba8acc6179a6ef5e68b0b6366c920b0
Ernests Dejus <ernisd621@gmail.com>
2022-12-15 22:53:41 +0200

    PHP request class

--- contact.php
+++ contact.php
@@ -1,16 +1,47 @@
 <?php
+class Request {
+  protected $requestType;
+  protected $requestUri;
+  public function __construct() {
+    $this->requestType = $_SERVER['REQUEST_METHOD'];
+    $this->requestUri = $_SERVER['REQUEST_URI'];
+  }
+  public function getRequestType() {
+    return $this->requestType;
+  }
+  public function getRequestUri() {
+    return $this->requestUri;
+  }
+  public function get($key) {
+    return isset($_GET[$key]) ? $_GET[$key] : null;
+  }
 
-$name = $_POST['name']
-$email = $_POST["sutitja-epasts"];
-$Subject = "Website"
+  public function post($key) {
+    return isset($_POST[$key]) ? $_POST[$key] : null;
+  }
+}
+
+$request = new Request();
+$requestType = $request->getRequestType();
+$requestUri = $request->getRequestUri();
+$username = $request->get('name');
+$password = $request->post('sutitaja-epasts');
+
+print_r($_GET);
+print_r($_POST);
+$firstname = $_GET['name'];
+$email3 = $_GET['sutitaja-epasts'];
+$name = $_POST['name'];
+$subject = "Website";
 $message = $_POST["sutitaja-zina"];
 
-$mailheader = "From:".$name."<".$email.">\r\n"
+echo $firstname;
+echo $lastname;
+
+$mailheader = "From:".$firstname."<".$email3.">\r\n";
 
 $recipient = "ernisd621@gmail.com";
 
-mail($recipent, $subject,$message,$mailheader)
+mail($recipient, $subject, $message, $mailheader)
 or die("Error!");
-
-
 ?>
